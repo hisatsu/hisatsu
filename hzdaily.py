2cf136a1beb2e08ddba751df0ec860af52e41b10
hisatsu <hisatsu@qq.com>
2017-04-25 02:28:29 +0800

    hzdaily add store method

--- hzdaily.py
+++ hzdaily.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re, urllib, datetime, csv
+import re
+import urllib
+import datetime
+import pymysql
 from urllib import request
 from urllib import error
-# 1.todo 考虑网络出错时的处理 2.结果存入excel|mysql
-
-# config
-headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-}
-# 匹配一手房数据
-pattern1 = re.compile(r'全市签约.*其中住宅(\d+)套')
-# match old house data
-pattern2 = re.compile(r'摘要.*其中住宅签约(\d+)套')
-# get data from webpage and return NUM
-
-# strftime format time
-delta = datetime.timedelta(days=1)
-start_date = datetime.date(2017, 4, 20)
-# start = str(start_date.strftime('%Y%m%d'))
-end_date = datetime.date.today() # - delta
-# end = str(end_date.strftime('%Y%m%d'))
 
 
+start_date = datetime.date(2016, 1, 1)
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='1q2w3e', db='mysql', charset='utf8' )
+cur = conn.cursor()
+cur.execute("USE djcode")
 
-
-
-def gather(url):
+def getPages(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '}
     req = urllib.request.Request(url, headers=headers)
     try:
         response = urllib.request.urlopen(req)
@@ -35,47 +23,33 @@ def gather(url):
         response = None
     return response
 
-def numbers(response, pattern):
+def getData(response, pattern):
     data = response.read().decode('utf-8')
     return pattern.search(data).group(1)
 
-# 获取两个日期之间的日期 并由此拼出url 获取数据
-excel = []
-xf_dict = {}
-esf_dict = {}
-while start_date < end_date:
-    # start 数据需要刷新
-    date = start_date.strftime('%Y%m%d')
-    excel.append(date)
-    url_xf = 'http://www.tmsf.com/upload/report/mrhqbb/' + date + '/xf.html'
-    url_esf = 'http://www.tmsf.com/upload/report/mrhqbb/' + date + '/esf.html'
-    xfdata = gather(url_xf)
-    xf = numbers(xfdata, pattern1)
-    excel.append(xf)
-    # print(xf)
-    # print(date + '新房:' + gather(url_xf, pattern1))
-    esfdata = gather(url_esf)
-    esf = numbers(esfdata, pattern2)
-    excel.append(esf)
-    xf_dict[date] = xf
-    esf_dict[date] = esf
-    # print(esf)
-    # print(date + '二手:' + gather(url_esf, pattern2))
-    start_date += delta
-new_excel = [excel[i:i+3] for i in range(0, len(excel), 3)]
-print(new_excel)
-print(xf_dict)
-print(esf_dict)
+def store(date, xf, esf):
+    cur.execute("INSERT INTO hzDaily (date, xf, esf) VALUES (%s, %s, %s)", (date, xf, esf))
+    cur.connection.commit()
 
+try:
+    end_date = datetime.date.today()
+    delta = datetime.timedelta(days=1)
+    pattern1 = re.compile(r'全市签约.*其中住宅(\d+)套')
+    pattern2 = re.compile(r'摘要.*其中住宅签约(\d+)套')
+    while start_date < end_date:
+        date = start_date.strftime('%Y%m%d')
+        url_xf = 'http://www.tmsf.com/upload/report/mrhqbb/' + date + '/xf.html'
+        # print(url_xf)
+        url_esf = 'http://www.tmsf.com/upload/report/mrhqbb/' + date + '/esf.html'
+        # print(url_esf)
+        xfdata = getPages(url_xf)
+        esfdata = getPages(url_esf)
+        xf = getData(xfdata, pattern1)
+        esf = getData(esfdata, pattern2)
+        store(date, xf, esf)
+        print(date + 'done!')
+        start_date += delta
+finally:
+    cur.close()
+    conn.close()
 
-
-
-
-
-# class HzdailyCallback:
-#     def __init__(self):
-#         self.writer = csv.writer(open('hzdaily.csv', 'w'))
-#         self.fields = ('date', 'xf', 'esf')
-#         self.writer.writerow(self.fields)
-#
-#     def __call__(self, ):
