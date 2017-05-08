0827e0ce0db9f140745ce29a4f22438292408e89
hisatsu <hisatsu@qq.com>
2017-05-09 03:17:37 +0800

    selenium wait until

--- hz_hhgz.py
+++ hz_hhgz.py
@@ -3,6 +3,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import datetime
 import re
@@ -26,12 +29,12 @@ driver.find_element_by_name("submit").click()
 time.sleep(random.uniform(1, 2))
 # get personal page(mobiel = no), target uid
 # settings
-pid = '48208247'
+pid = '48209300'
 uid = '5310405'
-reply = [u'好好干。再回100贴明天的盒饭就有着落了。',
+reply = [u'好好干。再回100贴你明天的盒饭就有着落了。',
          u'再多回50帖，奖励1个烂虾；多回100帖，奖励1条臭鱼。都可以带回家给你老娘吃',
          u'又牵着你妈出来卖，还不马路上跪着求人接盘去，你那脏病破烂的房子白送也没人要吧，无论行情好坏。',
-         u'有手有脚的干什么不好，非要出来干这缺德事？',
+         u'你智商真的有问题，好好想想。不过估计你也想不通',
          u'5毛钱拿好，回去买顿好的。让你老娘也别再出去卖了。孤儿寡母也是不容易。'
         ]
 
@@ -50,28 +53,35 @@ while True:
         if int(pid_latest) > int(pid):
             # go to the latest reply
             pid = pid_latest
-            driver.find_element_by_xpath('//table/tbody/tr[3]/td/a').click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '//table/tbody/tr[3]/td/a')))
+            element.click()
             # switch to latest window
+            # time.sleep(random.uniform(2, 4))
             for handle in driver.window_handles:
                 driver.switch_to.window(handle)
             # click reply button
-            time.sleep(random.uniform(1, 2))
             table_id = 'pid' + pid_latest
             xpath_pid = '//*[@id="' + table_id + '"]/tbody/tr[4]/td[2]/div/div/em/a[1]'
-            driver.find_element_by_xpath(xpath_pid).click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, xpath_pid)))
+            element.click()
             # reply it
-            time.sleep(random.uniform(1, 2))
+            # time.sleep(random.uniform(1, 2))
             # switch to the rich text frame
             driver.switch_to.frame('e_iframe')
             driver.find_element_by_xpath("//body[@spellcheck='false']").send_keys(reply[random.randrange(len(reply))])
             driver.switch_to.parent_frame()
-            driver.find_element_by_name('replysubmit').click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.NAME, 'replysubmit')))
+            element.click()
             reply_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
             time.sleep(random.uniform(3, 4))
             driver.close()
             driver.switch_to.window(handle1)
             print(pid_latest + 'replyed@' + reply_time)
     except Exception as e:
+        print(e)
         time.sleep(2)
 
     time.sleep(random.uniform(5, 10))
--- hz_jenova.py
+++ hz_jenova.py
@@ -3,6 +3,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import datetime
 import re
@@ -19,20 +22,21 @@ chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("http://zzhzbbs.zjol.com.cn/member.php?mod=logging&action=login&mobile=1")
 time.sleep(random.uniform(1, 2))
-driver.find_element_by_name("username").send_keys(u't800')
+driver.find_element_by_name("username").send_keys(u't8oo')
 driver.find_element_by_name("password").send_keys(u'1q2w3e4r5t')
 driver.find_element_by_name("submit").click()
 # driver.find_element_by_class_name("grey").click()
 time.sleep(random.uniform(1, 2))
 # get personal page(mobiel = no), target uid
 # settings
-pid = '48208247'
+pid = '48209419'
 uid = '5302519'
-reply = [u'小伙子干的不错。再回100贴明天的盒饭就有着落了。',
+reply = [u'这货帽子缀着‘lv’，喜滋滋',
          u'再多回50帖，奖励1个烂虾；多回100帖，奖励1条臭鱼。都可以带回家给你老娘吃',
          u'又牵着你妈出来卖，还不马路上跪着求人接盘去，你那脏病破烂的房子白送也没人要吧，无论行情好坏。',
-         u'有手有脚的干什么不好，非要出来干这缺德事？',
-         u'5毛钱拿好，回去买顿好的。让你老娘也别再出去卖了。孤儿寡母也是不容易。'
+         u'你智商真的有问题，好好想想。不过估计你也想不通',
+         u'5毛钱拿好，回去买顿好的。让你老娘也别再出去卖了。孤儿寡母也是不容易。',
+         u'j回家抬头一看，门头上两个大字“翠顶”，不由得心旷神怡，连道“好字，好字！”',
         ]
 
 while True:
@@ -51,28 +55,34 @@ while True:
         if int(pid_latest) > int(pid):
             # go to the latest reply
             pid = pid_latest
-            driver.find_element_by_xpath('//table/tbody/tr[3]/td/a').click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '//table/tbody/tr[3]/td/a')))
+            element.click()
             # switch to latest window
             for handle in driver.window_handles:
                 driver.switch_to.window(handle)
             # click reply button
-            time.sleep(random.uniform(1, 2))
             table_id = 'pid' + pid_latest
             xpath_pid = '//*[@id="' + table_id + '"]/tbody/tr[4]/td[2]/div/div/em/a[1]'
-            driver.find_element_by_xpath(xpath_pid).click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, xpath_pid)))
+            element.click()
             # reply it
-            time.sleep(random.uniform(1, 2))
+            # time.sleep(random.uniform(1, 2))
             # switch to the rich text frame
             driver.switch_to.frame('e_iframe')
             driver.find_element_by_xpath("//body[@spellcheck='false']").send_keys(reply[random.randrange(len(reply))])
             driver.switch_to.parent_frame()
-            driver.find_element_by_name('replysubmit').click()
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.NAME, 'replysubmit')))
+            element.click()
             reply_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
             time.sleep(random.uniform(3, 4))
             driver.close()
             driver.switch_to.window(handle1)
             print(pid_latest + 'replyed@' + reply_time)
     except Exception as e:
+        print(e)
         time.sleep(2)
 
     time.sleep(random.uniform(5, 10))
